ac1b473bc851d637de4e10e9be6e5b149cb5bfb1
dichevia <49196046+dichevia@users.noreply.github.com>
2019-07-11 11:34:55 +0300

    Blog
    Register & Login

---
+++ SoftUniBlog/README.md
@@ -0,0 +1,4 @@
+.checkout
+=========
+
+A Symfony project created on July 10, 2019, 11:15 am.
---
+++ SoftUniBlog/src/SoftUniBlogBundle/Controller/UsersController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace SoftUniBlogBundle\Controller;
+
+use SoftUniBlogBundle\Entity\User;
+use SoftUniBlogBundle\Form\UserType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UsersController extends Controller
+{
+    /**
+     * @Route("register", name="user_register")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function registerAction(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()){
+            $password = $this->get('security.password_encoder')
+                ->encodePassword($user, $user->getPassword());
+
+            $user->setPassword($password);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute("security_login");
+        }
+        return $this->render("default/register.html.twig",['form'=>$form->createView()]);
+    }
+}
